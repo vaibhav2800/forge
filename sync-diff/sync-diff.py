ebfbef6178d1cea96b252034ef5e741be8abdc68
mihaib <mihaib@castor>
2012-08-23 20:55:25 +0300

    sync-diff: made dirs cmd line args

--- sync-diff/sync-diff.py
+++ sync-diff/sync-diff.py
@@ -1,14 +1,39 @@
 #! /usr/bin/env python3
 
-import subprocess, shlex, tempfile
+import argparse, subprocess, shlex, sys, tempfile
 
-dirPairs = [
-        ['/path/to/d1', '/mirror/of/d1'],
-        ['/path/to/d2', '/mirror/of/d2'],
-    ]
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Sync directory pairs')
+
+    parser.add_argument('dir', nargs='+', help='''Directory pairs to compare.
+            You must specify an even number of directories.''')
+
+    return parser.parse_args()
+
+
+def get_dir_pairs():
+    args = parse_args()
+    if len(args.dir) % 2 != 0:
+        print('Odd number of directories given (',
+                len(args.dir), '): ', args.dir,
+                sep='', file=sys.stderr)
+        sys.exit(1)
+
+    dirPairs = []
+    pair = []
+    for dirName in args.dir:
+        pair.append(dirName)
+        if len(pair) == 2:
+            dirPairs.append(pair)
+            pair = []
+
+    return dirPairs
+
 
 lsCmd = shlex.split('ls -AgGR --time-style="+%Y-%m-%d %H:%M:%S %z"')
 grepCmd = shlex.split('grep -v -e "^drwxr-xr-x " -e "^total [0-9][0-9]*$"')
+
 
 def write_tree_metadata(dirPath, outFile):
     lsProc = subprocess.Popen(lsCmd, stdout=subprocess.PIPE, cwd=dirPath)
@@ -20,7 +45,9 @@ def write_tree_metadata(dirPath, outFile):
         raise Exception("grep error code:", grepCode)
     outFile.flush()
 
+
 if __name__ == '__main__':
+    dirPairs = get_dir_pairs()
     for (d1, d2) in dirPairs:
         print(d1, d2, sep='\t')
         with tempfile.NamedTemporaryFile(prefix='sync-diff-') as f1, \
